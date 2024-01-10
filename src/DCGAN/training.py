94d8864dab2295d1b4509d7f96ef73be9eec5296
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-01-11 02:29:21 +0700

    wandb integration removed

--- src/DCGAN/training.py
+++ src/DCGAN/training.py
@@ -7,17 +7,10 @@ import torch.optim as optim
 import torchvision
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-import wandb
 from torch.utils.data import DataLoader
 
 from DCGAN.models import Discriminator, Generator, initialize_weights
 
-
-my_api_key: str = "add-secret-storage-here"
-wandb.login(key=my_api_key)
-run = wandb.init(
-    project="dcgan-mnist"
-)
 
 device: object = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 
@@ -62,8 +55,6 @@ criterion = nn.BCELoss()
 
 fixed_noise: torch.Tensor = torch.randn((32, z_dim, 1, 1)).to(device) # type: ignore
 
-step: int = 0
-
 for epoch in range(num_epochs):
     for batch_idx, (real, _) in enumerate(dataloader):
         real = real.to(device)
@@ -71,7 +62,7 @@ for epoch in range(num_epochs):
 
         fake = generator(noise)
 
-        # train discriminator max[log(D(x)) + log(1 - D(G(z)))]
+        # train discriminator: max[log(D(x)) + log(1 - D(G(z)))]
         discriminator_real = discriminator(real).reshape(-1)
         loss_discriminator_real = criterion(discriminator_real, torch.ones_like(discriminator_real))
 
@@ -84,7 +75,7 @@ for epoch in range(num_epochs):
         loss_discriminator.backward(retain_graph=True)
         optimizer_discriminator.step()
 
-        # train generator min[log(1 - D(G(z)))] <-> max[log(D(G(z)))]
+        # train generator: min[log(1 - D(G(z)))] <-> max[log(D(G(z)))]
         output = discriminator(fake).reshape(-1)
         loss_generator = criterion(output, torch.ones_like(output))
 
@@ -96,25 +87,3 @@ for epoch in range(num_epochs):
         if batch_idx == 0:
             print(f"Epoch [{epoch}/{num_epochs}] Loss D: {loss_discriminator:.4f}, " + 
                       f"loss G: {loss_generator:.4f}")
-
-            with torch.no_grad():
-                fake = generator(fixed_noise)
-                    
-                img_grid_fake = torchvision.utils.make_grid(fake[:32], normalize=True)
-                img_grid_real = torchvision.utils.make_grid(real[:32], normalize=True)
-
-                wandb.log(
-                    {
-                        "loss Discriminator": loss_discriminator,
-                        "loss Generator": loss_generator
-                    }, step=step
-                )
-
-                wandb.log(
-                    {
-                        "images fake": wandb.Image(img_grid_fake),
-                        "images real": wandb.Image(img_grid_real)
-                    }, step = step
-                )
-
-                step += 1
