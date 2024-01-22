ba84db212af637d0df0e580eeae7ae934313a579
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-01-22 15:31:11 +0300

    WGAN submodule updated with ruff

--- src/WGAN/models.py
+++ src/WGAN/models.py
@@ -3,16 +3,19 @@ import torch.nn as nn
 
 
 class Critic(nn.Module):
+    """
+    Critic model. Based on DCGAN, except last layer: critic model doesn't have last activation layer;
+    """
     def __init__(self, channels_image: int, features_d: int) -> None:
         super(Critic, self).__init__()
         
         self.critic = nn.Sequential(
-            nn.Conv2d(channels_image, features_d, kernel_size=4, stride=2, padding=1), # 64x64 ->x32x32
+            nn.Conv2d(channels_image, features_d, kernel_size=4, stride=2, padding=1),  # 64x64 ->x32x32
             nn.LeakyReLU(0.2),
-            self._block(features_d, features_d*2, 4, 2, 1), # 32x32 -> 16x16
-            self._block(features_d*2, features_d*4, 4, 2, 1), # 16x16 -> 8x8
-            self._block(features_d*4, features_d*8, 4, 2, 1), # 8x8 -> 4x4
-            nn.Conv2d(features_d*8, 1, kernel_size=4, stride=1, padding=0)
+            self._block(features_d, features_d * 2, 4, 2, 1),  # 32x32 -> 16x16
+            self._block(features_d * 2, features_d * 4, 4, 2, 1),  # 16x16 -> 8x8
+            self._block(features_d * 4, features_d * 8, 4, 2, 1),  # 8x8 -> 4x4
+            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=1, padding=0)
         )
 
     def _block(self, in_channels: int, out_channels: int, kernel_size: int,
@@ -36,16 +39,19 @@ class Critic(nn.Module):
     
 
 class Generator(nn.Module):
+    """
+    Generator model. Based on DCGAN;
+    """
     def __init__(self, z_dim: int, channels_image: int, features_g: int):
         super(Generator, self).__init__()
 
         self.generator = nn.Sequential(
-            self._block(z_dim, features_g*16, 4, 1, 0), # z_dimx1x1 -> features_g*16x4x4
-            self._block(features_g*16, features_g*8, 4, 2, 1), # 4x4 -> 8x8
-            self._block(features_g*8, features_g*4, 4, 2, 1), # 8x8 -> 16x16
-            self._block(features_g*4, features_g*2, 4, 2, 1), # 16x16 -> 32x32
-            nn.ConvTranspose2d(features_g*2, channels_image,
-                kernel_size=4, stride=2, padding=1), # 32x32 -> 64x64
+            self._block(z_dim, features_g * 16, 4, 1, 0),  # z_dimx1x1 -> features_g*16x4x4
+            self._block(features_g * 16, features_g * 8, 4, 2, 1),  # 4x4 -> 8x8
+            self._block(features_g * 8, features_g * 4, 4, 2, 1),  # 8x8 -> 16x16
+            self._block(features_g * 4, features_g * 2, 4, 2, 1),  # 16x16 -> 32x32
+            nn.ConvTranspose2d(features_g * 2, channels_image,
+                kernel_size=4, stride=2, padding=1),  # 32x32 -> 64x64
             nn.Tanh(), 
         )
 
@@ -70,6 +76,12 @@ class Generator(nn.Module):
 
 
 def initialize_weights(model: nn.Module) -> None:
+    """
+    Weights initialization poroposed in the paper;
+
+    Args:
+        model (nn.Module): model which weights you want to initialize;
+    """
     for module in model.modules():
         if isinstance(module, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
             nn.init.normal_(module.weight.data, 0.0, 0.02)
--- src/WGAN/training.py
+++ src/WGAN/training.py
@@ -2,9 +2,7 @@ import os
 from pathlib import Path
 
 import torch
-import torch.nn as nn
 import torch.optim as optim
-import torchvision
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
